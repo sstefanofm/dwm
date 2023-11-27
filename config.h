67dab806bd6ac5e3863e1f5b71425efea1036e6b
stf <sstefanofm@gmail.com>
2023-11-26 21:47:03 -0300

    hpitems

--- config.h
+++ config.h
@@ -60,7 +60,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-c", "-l", "7", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char hpitems[] = "firefox,ungoogled-chromium,color-picker,codium,vim,neovim,librewolf,telegram,simplescreenrecorder";
+static const char *dmenucmd[] = { "dmenu_run", "-c", "-l", "7", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-hp", hpitems, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
