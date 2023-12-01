a53d6e5db156718b7354af731cc919cfcc24bdf5
stf <sstefanofm@gmail.com>
2023-11-30 21:35:01 -0300

    ungoogled-chromium -> chromium. st -> kitty.

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -60,9 +60,9 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char hpitems[] = "firefox,ungoogled-chromium,color-picker,codium,vim,neovim,librewolf,telegram,simplescreenrecorder";
+static const char hpitems[] = "spotify,firefox,chrome,color-picker,codium,vim,neovim,librewolf,telegram,simplescreenrecorder";
 static const char *dmenucmd[] = { "dmenu_run", "-c", "-l", "7", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-hp", hpitems, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
