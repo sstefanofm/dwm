445dc827c9b44228e791e013f9d52d3cdf0c17e0
stf <sstefanofm@gmail.com>
2024-05-14 22:04:17 -0300

    cool delimiter & cool mem & cool blocks.h

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^lol/ { print $3\" / \"$2\"  \" }' | sed s/i//g",	30,		0},
+	{"  ", "free -h | awk '/^Mem/ { print $3\" / \"$2 }' | sed s/i//g",	30,		0},
 	{"", "date.sh",					900,		0},
   {"", "batt.sh",         120,    0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " r ";
+static char delim[] = "  ";
 static unsigned int delimLen = 10;
