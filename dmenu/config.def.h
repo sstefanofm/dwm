8e4898f259954e6c26dae859f397dd99ec8fe5cb
stf <sstefanofm@gmail.com>
2024-06-01 19:04:07 -0300

    alpha patch and line height patch

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -1,31 +1,30 @@
-/* See LICENSE file for copyright and license details. */
-/* Default settings; can be overriden by command line. */
+static int topbar                = 1;    /* -b  option; if 0, dmenu appears at bottom */
+static int centered              = 1;    /* -c option; centers dmenu on screen */
+static int min_width             = 400;  /* minimum width when centered */
+static const int user_bt         = 9;    /* add an defined amount of pixels to the bar height */
+static unsigned int lines        = 6;    /* -l option; vertical list with given number of lines */
+static unsigned int border_width = 2;    /* size of the window border */
+static const unsigned int alpha  = 0xb9; /* alpha transparency; 0xff is OPAQUE */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
-/* -fn option overrides fonts[0]; default X11 font or font set */
-static const int user_bt = 0;               /* add an defined amount of pixels to the bar height */
-
+static const char *prompt = NULL; /* -p option; prompt to the left of input field */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"SpaceMono Nerd Font:size=11:style=Regular"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-	[SchemeHp]  = { "#bbbbbb", "#333333" }
+	[SchemeSel]  = { "#eeeeee", "#005577" },
+	[SchemeOut]  = { "#000000", "#00ffff" },
+	[SchemeHp]   = { "#bbbbbb", "#333333" }
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static const unsigned int alphas[SchemeLast][2] = {
+	[SchemeNorm] = { OPAQUE, alpha },
+	[SchemeSel]  = { OPAQUE, alpha },
+	[SchemeOut]  = { OPAQUE, alpha },
+};
 
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
-
-/* Size of the window border */
-static unsigned int border_width = 0;
