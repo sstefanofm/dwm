f77f308cbc90e61c9b03dec0dfac796ed96ef4a4
stf <sstefanofm@gmail.com>
2024-06-11 00:28:21 -0300

    fullpath to execute scripts

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,9 +1,9 @@
 static const Block blocks[] = {
-  /* Icon */ /* Command */ /* Update Interval */ /* Update Signal */
-  { "  ",   "memo.sh",	   5,                    0 },
-  { "  ",   "date.sh",    900,                  0 },
-  { "󰅒  ",   "hour.sh",    60,                   0 },
-  { "󱐋 ",    "batt.sh",    120,                  0 },
+  /* Icon */ /* Command */                  /* Update Interval */ /* Update Signal */
+  { "  ",   "/home/stf/.local/bin/memo.sh",	  5,                    0 },
+  { "  ",   "/home/stf/.local/bin/date.sh",    900,                  0 },
+  { "󰅒  ",   "/home/stf/.local/bin/hour.sh",    60,                   0 },
+  { "󱐋 ",    "/home/stf/.local/bin/batt.sh",    120,                  0 },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
