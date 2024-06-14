6b23080b4ea9be10642a5dfdd5c800147c31f7b6
stf <sstefanofm@gmail.com>
2024-06-14 18:40:48 -0300

    match border-width

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -5,7 +5,7 @@ static int centered              = 1;    /* -c option; centers dmenu on screen *
 static int min_width             = 400;  /* minimum width when centered */
 static const int user_bt         = 19;   /* add an defined amount of pixels to the bar height */
 static unsigned int lines        = 4;    /* -l option; vertical list with given number of lines */
-static unsigned int border_width = 2;    /* size of the window border */
+static unsigned int border_width = 1;    /* size of the window border */
 static const unsigned int alpha  = 0xb9; /* alpha transparency; 0xff is OPAQUE */
 
 static const Theme theme = th_pink_lain;
