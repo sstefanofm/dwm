68b83861b61688d06971ca92231d8c8fbf0e8401
stf <sstefanofm@gmail.com>
2024-09-12 19:58:47 -0300

    update batt.sh script

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,9 +1,9 @@
 static const Block blocks[] = {
-  /* Icon */ /* Command */                  /* Update Interval */ /* Update Signal */
-  { "  ",   "memo.sh",	  5,                    0 },
+  /* Icon */ /* Command */ /* Update Interval */ /* Update Signal */
+  { "  ",   "memo.sh",	   5,                    0 },
   { "  ",   "date.sh",    900,                  0 },
   { "󰅒  ",   "hour.sh",    60,                   0 },
-  { "󱐋 ",    "batt.sh",    120,                  0 },
+  { "",      "batt.sh",    120,                  1 },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
